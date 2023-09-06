29f2e94bb4bfdfe62ccdd8c534082aefedfcd573
jdwdonga21 <ongame21212144@gmail.com>
2023-09-06 17:28:17 +0900

    230906 테스트용 완성

--- expandability_test.py
+++ expandability_test.py
@@ -8,41 +8,57 @@ import time
 
 
 url = "https://api.bell.zeroweb.kr/health-check?db=1"
+urlTest = "http://127.0.0.1:5000/health-check"
 payload = {'db': '1'}
 errorTimes = 0 #마지막error발생시간, log에 찍히는 시간
 normalTimes = 0 #오류 수정 시간 계산을 위한 정상회복된 시간
 errorCorrectionTime = 0 #오류수정 시간
+firstErrorTime = True # 첫 오류 발생
 countNormalNum = 3 # 정상회복 횟수 0~3
 runStart = 0 #1회 동작 시작시간
 runEnd = 0 #1회 동작 종료시간
 
 
 def getHttp():
-    global countNormalNum, errorTimes, normalTimes
-    response = requests.get(url)    
+    global countNormalNum, errorTimes, normalTimes, firstErrorTime
+    response = requests.get(urlTest)    
     if response.status_code >= 200 and response.status_code < 300:
         if countNormalNum == 3:            
             print("정상")
             print("응답코드 : " + str(response.status_code))            
             normalTimes = time.time()
-            korTimes = time.strftime('%x %X')
+            korTimes = time.strftime('%Y-%m-%d'+ ' ' +'%X')
             resCode = response.status_code
-            print("시간 : " + str(normalTimes))
+            print("TimeStamp : " + str(int(normalTimes)))
             print("한국시간 : " + str(korTimes))
             printLog(1, normalTimes, korTimes, resCode, 0)
         else:
+            if countNormalNum != 2:
+                print("정상(오류 수정 중)")
+                print("응답코드 : " + str(response.status_code))            
+                normalTimes = time.time()
+                korTimes = time.strftime('%Y-%m-%d'+ ' ' +'%X')
+                resCode = response.status_code
+                print("TimeStamp : " + str(int(normalTimes)))
+                print("한국시간 : " + str(korTimes))
+                printLog(1, normalTimes, korTimes, resCode, 0)
             countNormalNum = countNormalNum + 1
             if countNormalNum == 3:
+                firstErrorTime = True
                 # 오류 수정타임
                 normalTimes = time.time()
-                korTimes = time.strftime('%x %X')
+                korTimes = time.strftime('%Y-%m-%d'+ ' ' +'%X')
                 resCode = response.status_code
-                errorCorrectionTime = normalTimes - errorTimes
-                print("정상(오류 수정)")
+                #errorCorrectionTime = normalTimes - errorTimes
+                errorCorrectionTimeMs = int((normalTimes - errorTimes) * 1000)
+                errorCorrectionTimeSec = round(normalTimes - errorTimes, 1)
+                print("정상(오류 수정 완료)")
                 print("응답코드 : " + str(response.status_code))
-                print("시간 : " + str(normalTimes))
+                print("TimeStamp : " + str(int(normalTimes)))
                 print("한국시간 : " + str(korTimes))
-                print("오류 수정 시간 : " + str(errorCorrectionTime))
+                # 000초 (0000(ms))
+                errorCorrectionTime = str(errorCorrectionTimeSec) + "Sec" + '(' + str(errorCorrectionTimeMs) + '(ms)' + ')'
+                print("오류 수정 시간 : " + str(errorCorrectionTimeSec) + "Sec" + '(' + str(errorCorrectionTimeMs) + '(ms)' + ')')
                 # 로그 파일 작성
                 printLog(0, normalTimes, korTimes, resCode, errorCorrectionTime)
     else:
@@ -50,26 +66,33 @@ def getHttp():
         print("비정상")
         print("응답코드 : " + str(response.status_code))
         # 에러 시간 측정
-        errorTimes = time.time() 
-        korTimes = time.strftime('%x %X')    
+        errorTimes = time.time()
+        korTimes = time.strftime('%Y-%m-%d'+ ' ' +'%X')
         resCode = response.status_code   
         # 에러 발생시 count 는 0
         countNormalNum = 0
-        print("시간 : " + str(errorTimes))
+        print("TimeStamp : " + str(int(errorTimes)))
         print("한국시간 : " + str(korTimes))
         # 로그 파일 작성
-        printLog(1, errorTimes, korTimes, resCode, 0)    
+        if firstErrorTime:
+            firstErrorTime = False
+            printLog(2, errorTimes, korTimes, resCode, 0)    
+        else:
+            printLog(1, errorTimes, korTimes, resCode, 0)
     print("------- - ------- - -------")
 
 
 def printLog(_result, _times, _kortimes, _code, _ectimes):    
+    timeStamp = int(_times)
     log_message = ""
     # 실패 ->성공
     if _result == 0:
-        log_message = f"UTC: {_times} Korean Time: {_kortimes} Code: {_code} Error correction time: {_ectimes}\n"
+        log_message = f"TimeStamp: {timeStamp} KST : {_kortimes} Code: {_code} Error correction time: {_ectimes}\n"
+    elif _result == 2:
+        log_message = f"TimeStamp: {timeStamp} KST : {_kortimes} Code: {_code} StartError\n"
     # 성공 or 실패                    
     else:
-        log_message = f"UTC: {_times} Korean Time: {_kortimes} Code: {_code}\n"
+        log_message = f"TimeStamp: {timeStamp} KST : {_kortimes} Code: {_code}\n"
     #log 파일 작성     
     with open("expandability_test_result.log", "a") as f:
         f.write(log_message)
@@ -78,5 +101,6 @@ while True:
     getHttp()
     runEnd = time.time()
     runtime = runEnd - runStart
+    print("runtime : " + str(runtime))
     if runtime <= 0.1:
         time.sleep(0.1 - runtime)
